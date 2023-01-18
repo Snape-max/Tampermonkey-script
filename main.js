4eab7e2e712e99b32a734cad67e2dc8fb48d3c1d
Snape-max <69849470+Snape-max@users.noreply.github.com>
2023-01-18 21:53:59 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+if($("div.header.navbar-fixed-top").length>0)
+{
+    $("div.header.navbar-fixed-top").remove();
+}
+if($("div.download").length>0)
+{
+    $("div.download").remove();
+}
+if($("div.footer").length>0)
+{
+    $("div.footer").remove();
+}
+if($("a.read-more").length>0)
+{
+    $("a.read-more").remove();
+}
+if($("div.Daily").length>0)
+{
+    $("div.Daily").remove();
+}
+if($("a.view-more").length>0)
+{
+    $("a.view-more").remove();
+}
+
+
